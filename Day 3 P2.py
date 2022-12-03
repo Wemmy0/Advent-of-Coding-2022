26b6822b54be098ba3afe4ffdb89a45071268311
thomas <thomash2505@pm.me>
2022-12-03 11:10:20 +0000

    Completed Day 3

---
+++ Day 3 P2.py
@@ -0,0 +1,19 @@
+import string
+
+end = []
+lower_alphabet = string.ascii_lowercase
+upper_alphabet = string.ascii_uppercase
+with open('Day 3 Input') as file:
+    lines = file.readlines()
+    count = 0
+    for i in range(0, len(lines), 3):
+        first = set(lines[i+0].strip("\n"))
+        second = set(lines[i+1].strip("\n"))
+        third = set(lines[i+2].strip("\n"))
+        badge = list(first.intersection(second).intersection(third))[0]
+        if badge in lower_alphabet:
+            end.append(lower_alphabet.index(badge)+1)
+        else:
+            end.append(upper_alphabet.index(badge)+27)
+
+print(sum(end))
---
+++ Day 3.py
@@ -0,0 +1,22 @@
+import string
+
+end = []
+lower_alphabet = string.ascii_lowercase
+upper_alphabet = string.ascii_uppercase
+with open('Day 3 Input') as file:
+    lines = file.readlines()
+    count = 0
+    for i in lines:
+        compartment_1 = i[:int(len(i) / 2)]
+        compartment_2 = i[int(len(i) / 2):].strip("\n")
+        counter = 0
+        for i in compartment_1:
+            if i in compartment_2:
+                if i in lower_alphabet:
+                    end.append(lower_alphabet.index(i)+1)
+                    break
+                else:
+                    end.append(upper_alphabet.index(i)+27)
+                    break
+
+print(sum(end))
