067fb2b5eb4884bf143bc00c582c99f6266a88c7
thomas <thomash2505@pm.me>
2022-12-07 15:21:37 +0000

    Day 6

---
+++ Day 6.py
@@ -0,0 +1,21 @@
+with open('Day 6 Input') as file:
+    line = file.readline().strip("\n")
+
+def find(num):
+    tmp = []
+    for i in range(len(line)):
+        tmp.append(line[i])
+        if len(tmp) == num+1:
+            tmp.pop(0)
+        if len(tmp) == num:
+            duplicates = [character for character in tmp if tmp.count(character) > 1]
+            if len(duplicates) != 0:
+                pass
+            else:
+                out = ""
+                for i in tmp:
+                    out += i
+                break
+    return line.index(out)+num
+print(find(4))
+print(find(14))
